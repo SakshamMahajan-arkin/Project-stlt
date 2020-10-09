d611b70642548eff66bf3df6e1c237136f7938db
jvmolu <anmol.singhal50@gmail.com>
2020-10-09 22:55:51 +0530

    updated header

--- tutor/registration.php
+++ tutor/registration.php
@@ -32,20 +32,23 @@
 
            <div class="collapse navbar-collapse" id="navbarSupportedContent">
                <ul class="navbar-nav ml-auto">
-                   <li class="nav-item active ">
+                   <li class="nav-item ">
                        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
                    </li>
                    <li class="nav-item">
                        <a class="nav-link" href="#">About US</a>
                    </li>
                    <li class="nav-item">
-                       <a class="nav-link" href="#">Register</a>
+                       <a class="nav-link active" href="#">Register</a>
                    </li>
                    <li class="nav-item">
                        <a class="nav-link" href="#">Feedback</a>
                    </li>
                    <li class="nav-item">
                        <a class="nav-link" href="#">Contact Us</a>
+                   </li>
+                   <li class="nav-item">
+                     <a class="nav-link" href="#">Newsletter</a>
                    </li>
                </ul>
            </div>
