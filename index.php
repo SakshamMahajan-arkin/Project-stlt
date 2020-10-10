9d688866706e1134a86307525d8283e6b9eb66a0
mohitv02 <mohitverma11s3@gmail.com>
2020-10-10 23:05:32 +0530

    some more small changes

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
+    <link href="https://fonts.googleapis.com/css2?family=Comfortaa:wght@300&display=swap" rel="stylesheet">
     <!-- Bootstrap CSS -->
     <link href="https://fonts.googleapis.com/css2?family=Balsamiq+Sans&family=Chelsea+Market&family=Nunito+Sans:ital,wght@1,600&family=Ubuntu&family=Yanone+Kaffeesatz:wght@300&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
